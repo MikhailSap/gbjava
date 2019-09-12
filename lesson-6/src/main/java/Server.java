7a1f402104c4084f839c1b28d7c6cebbb1091952
Mikhail <vandaniel@mail.ru>
2019-09-12 12:52:24 +0300

    lesson-6 v.1

---
+++ lesson-6/src/main/java/Server.java
@@ -0,0 +1,74 @@
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Server {
+    private final int PORT = 5000;
+    private ServerSocket serverSocket;
+    private Socket connection;
+    private DataInputStream receive;
+    private DataOutputStream send;
+    private Scanner scanner = new Scanner(System.in);
+    private Thread thredForSend;
+
+    public Server() {
+        try {
+            openConnect();
+            thredForSend.join();
+            send.close();
+            receive.close();
+            connection.close();
+            serverSocket.close();
+            System.out.println("Server is closed");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void openConnect() throws IOException{
+        serverSocket = new ServerSocket(PORT);
+        connection = serverSocket.accept();
+        receive = new DataInputStream(connection.getInputStream());
+        send = new DataOutputStream(connection.getOutputStream());
+        thredForSend = new Thread(() -> {
+            String content;
+            while (true) {
+                content = scanner.nextLine();
+                if (content.equals("quit")) {
+                    try {
+                        send.writeUTF(content);
+                        System.out.println("send is closed");
+                        break;
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+                try {
+                    send.writeUTF(content);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        thredForSend.start();
+        receive();
+    }
+
+    public void receive() throws IOException {
+        String content;
+        while (true) {
+            content = receive.readUTF();
+            if (content.equals("quit")) {
+                System.out.println("receive is closed");
+                break;
+            }
+            System.out.println(content);
+        }
+    }
+
+    public static void main(String[] args) {
+        new Server();
+    }
+}
