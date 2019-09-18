ee1368424fef429f60cdd4460e530adbbc506bac
Mikhail <vandaniel@mail.ru>
2019-09-18 23:15:26 +0300

    lesson-8 v.1

---
+++ lesson-8/src/main/java/server/AuthService.java
@@ -0,0 +1,5 @@
+package server;
+
+public interface AuthService {
+    Member checkAuthData(String login, String pass);
+}
---
+++ lesson-8/src/main/java/server/BaseAuthService.java
@@ -0,0 +1,15 @@
+package server;
+
+import java.util.concurrent.ConcurrentLinkedQueue;
+
+public class BaseAuthService implements AuthService {
+    private ConcurrentLinkedQueue<Member> members = new ChatMembers().getMembers();
+
+    public Member checkAuthData(String login, String pass) {
+        for (Member member : members) {
+            if (member.getLogin().equals(login) && member.getPass().equals(pass))
+                return member;
+        }
+        return null;
+    }
+}
